4310e518b600017a744503a580ecb0373f1eb1f3
Aru Sahni <aru@osohq.com>
2025-12-23 08:11:47 -0500

    lsp support

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,28 @@
-use zed_extension_api as zed;
+use zed_extension_api::{self as zed, Result, process::Command};
 
 struct PolarExtension {}
+
+fn get_lsp_binary_path(worktree: &zed_extension_api::Worktree) -> Result<String> {
+    worktree
+        .which("oso-cloud")
+        .ok_or_else(|| "no oso-cloud binary found".to_string())
+}
 
 impl zed::Extension for PolarExtension {
     fn new() -> Self {
         PolarExtension {}
+    }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed_extension_api::LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> zed_extension_api::Result<zed_extension_api::Command> {
+        Ok(Command {
+            command: get_lsp_binary_path(worktree)?,
+            args: vec![String::from("lsp")],
+            env: Default::default(),
+        })
     }
 }
 
