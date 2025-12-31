3c5650dcff13a4f0c23335dbb7b499d7dcbe21cc
Aru Sahni <aru@osohq.com>
2025-12-31 15:20:53 -0500

    Add outline queries.

---
+++ languages/polar/outline.scm
@@ -0,0 +1,7 @@
+(resource_block (resource_type) @name name: (namespaced_identifier) @context) @item
+(declaration ["permissions" "roles" "relations"] @name) @item
+(test_block header: (test_header name: (string)) @name) @item
+(test_setup "setup" @name) @item
+(test_fact name: (namespaced_identifier) @name) @item
+(assertion keyword: ["assert" "assert_not"] @name predicate: (_) @context) @item
+(rule_block (rule_functor) @name) @item
