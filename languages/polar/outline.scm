9b467c5bacce3f6c0b872574443b79b7e764e66f
Aru Sahni <aru@osohq.com>
2026-03-19 09:15:11 -0400

    Update tree-sitter grammar

--- languages/polar/outline.scm
+++ languages/polar/outline.scm
@@ -2,6 +2,6 @@
 (declaration ["permissions" "roles" "relations"] @name) @item
 (test_block header: (test_header name: (string)) @name) @item
 (test_setup "setup" @name) @item
-(test_fact name: (namespaced_identifier) @name) @item
+(fact_declaration name: (namespaced_identifier) @name) @item
 (assertion keyword: ["assert" "assert_not"] @name predicate: (_) @context) @item
 (rule_block (rule_functor) @name) @item
